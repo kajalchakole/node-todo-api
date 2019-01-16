a1041ae2230ad39f8a53fcfbfbcb9b1666154b55
kc <kajalchakole@yahoo.co.in>
2019-01-16 15:21:23 +0530

    Added Todo:id test cases

--- server/models/user.js
+++ server/models/user.js
@@ -1,26 +1,12 @@
 var mongoose = require('mongoose');
 
 var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 });
 
-
-// var user = new User({
-//     email: 'abc@gmail.com'
-// });
-
-// user.save().then((doc) => {
-//     console.log('User saved');
-    
-// }, (err) => {
-//     console.log('Unable to save user', err);
-    
-// });
-
-
-module.exports = {User};
+module.exports = {User}
