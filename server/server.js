e3df946c7d7c5306a14c0832133075843b58e18c
Kajal C <kajalchakole@yahoo.co.in>
2019-01-22 00:39:32 +0530

    modified env PORT

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ var {ToDo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
