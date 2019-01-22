a2e22faa1cfa96ba3615e776f6c6ec1a3accfbad
Kajal C <kajalchakole@yahoo.co.in>
2019-01-23 00:19:03 +0530

    Store hashed password into DB with mongoose routes

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,21 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password,salt, (err, hash) => {
+        console.log(hash);  
+              
+    });
+});
+
+var hashedPassword = '$2a$10$yDgBtMa0Dc6MMA2WqyJqVeaxCn.q7ys8cJrTgZeSWZi443BTGcu8K';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);    
+});
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
@@ -8,30 +24,30 @@ const jwt = require('jsonwebtoken');
 // console.log(`Hash: ${hash}`);
 
 
-var data = {
-    id:  10
-};
-var token = jwt.sign(data, 'abc123');
+// var data = {
+//     id:  10
+// };
+// var token = jwt.sign(data, 'abc123');
 
-console.log(token);
+// console.log(token);
 
-var decoded = jwt.verify(token, 'abc123');
-console.log(decoded);
+// var decoded = jwt.verify(token, 'abc123');
+// console.log(decoded);
 
 
-var token = {
-    data,
-    hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-};
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
 
-token.data = 5;
-token.hash = SHA256(JSON.stringify(token.data)).toString();
+// token.data = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
 
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
 
-if(resultHash === token.hash) {
-    console.log('Data was not changed');    
-} else {
-    console.log('Data was changed. Do not trust!');    
-}
+// if(resultHash === token.hash) {
+//     console.log('Data was not changed');    
+// } else {
+//     console.log('Data was changed. Do not trust!');    
+// }
 
